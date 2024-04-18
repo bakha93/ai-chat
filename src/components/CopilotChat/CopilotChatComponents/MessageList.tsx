59918d1472bb5e11b7e57315f3613acb3deb4fdd
bakha93 <vladbakhtir939@gmail.com>
2024-04-18 16:48:29 +0300

    feat: auto scroll on new message

--- src/components/CopilotChat/CopilotChatComponents/MessageList.tsx
+++ src/components/CopilotChat/CopilotChatComponents/MessageList.tsx
@@ -3,13 +3,22 @@ import { UserMessage } from "@/components/CopilotChat/CopilotChatComponents/User
 import { ChatGptMessage } from "@/components/CopilotChat/CopilotChatComponents/ChatGPTMessage.tsx";
 import textingGif from "@/assets/typing-texting.gif";
 import styles from './copilotChat.module.css'
+import {useEffect, useRef} from "react";
 
 export const MessageList = () => {
   const messages = useChatStore((state) => state.messages);
   const isMessageLoading = useChatStore((state) => state.isMessageLoading);
+  const listRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    // auto scroll on new message
+    if(listRef.current){
+      listRef.current.scrollTop = listRef.current.scrollHeight;
+    }
+  },[messages])
 
   return (
-    <div className={styles.messageList}>
+    <div ref={listRef} className={styles.messageList}>
       {messages.map((message, index) => {
         if (message.role === "user") {
           return (
